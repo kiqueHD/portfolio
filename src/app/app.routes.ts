cef86aa3bf98026d96c3d0f1beb64ae38f7804bb
kique <soyenriqueh@hotmail.com>
2025-12-20 21:04:02 +0100

    estructura inicial componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
+import { Timeline } from './components/timeline/timeline';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'about', component: About },
+    { path: 'projects', component: Projects },
+    { path: 'timeline', component: Timeline }
+];
