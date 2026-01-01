23bb0aecf60dc721ef1a44cbdadbb448a42ee996
Enrique <soyenriqueh@hotmail.com>
2026-01-01 19:07:17 +0100

    del: about component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { About } from './components/about/about';
 import { Projects } from './pages/projects/projects';
 import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'about', component: About },
     { path: 'projects', component: Projects },
     { path: 'contact', component: Contact },
     { path: '**', redirectTo: '' }
